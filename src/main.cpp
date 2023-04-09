8a571b565a0e3055e07f786b8105025153f31658
thunder <aoki.eita1130@gmail.com>
2023-04-09 16:54:54 +0900

    Animalが動く

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,45 @@
 #define STRINGIFY(x) #x
 #define MACRO_STRINGIFY(x) STRINGIFY(x)
 
-int add(int i, int j) {
+int add(int i, int j)
+{
     return i + j;
 }
 
+class Animal
+{
+public:
+    virtual ~Animal() {}
+    virtual std::string go(int n_times) = 0;
+};
+
+std::string call_go(Animal *animal)
+{
+    return animal->go(3);
+}
+
+class PyAnimal : public Animal
+{
+public:
+    /* Inherit the constructors */
+    using Animal::Animal;
+
+    /* Trampoline (need one for each virtual function) */
+    std::string go(int n_times) override
+    {
+        PYBIND11_OVERRIDE_PURE(
+            std::string, /* Return type */
+            Animal,      /* Parent class */
+            go,          /* Name of function in C++ (must match Python name) */
+            n_times      /* Argument(s) */
+        );
+    }
+};
+
 namespace py = pybind11;
 
-PYBIND11_MODULE(thun_search, m) {
+PYBIND11_MODULE(thun_search, m)
+{
     m.doc() = R"pbdoc(
         Pybind11 example plugin
         -----------------------
@@ -29,12 +61,20 @@ PYBIND11_MODULE(thun_search, m) {
         Some other explanation about the add function.
     )pbdoc");
 
-    m.def("subtract", [](int i, int j) { return i - j; }, R"pbdoc(
+    m.def(
+        "subtract", [](int i, int j)
+        { return i - j; },
+        R"pbdoc(
         Subtract two numbers
 
         Some other explanation about the subtract function.
     )pbdoc");
 
+    py::class_<Animal, PyAnimal>(m, "Animal")
+        .def(py::init<>())
+        .def("go", &Animal::go);
+
+    m.def("call_go", &call_go);
 #ifdef VERSION_INFO
     m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
 #else
