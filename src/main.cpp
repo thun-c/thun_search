9b44019151f922a282db7863dca51549469e891b
thunder <aoki.eita1130@gmail.com>
2023-04-10 01:07:56 +0900

    変数に代入できる

--- src/main.cpp
+++ src/main.cpp
@@ -38,6 +38,75 @@ public:
     }
 };
 
+class State
+{
+private:
+public:
+    double evaluated_score_ = 0; // 探索上で評価したスコア
+    int first_action_ = -1;      // 探索木のルートノードで最初に選択した行動
+    virtual ~State() {}
+
+    // ゲームの終了判定
+    virtual bool isDone() = 0;
+
+    // 探索用の盤面評価をする
+    virtual void evaluateScore() = 0;
+
+    virtual void setEvaluateScore(double evaluated_score)
+    {
+        this->evaluated_score_ = evaluated_score;
+    }
+
+    // 指定したactionでゲームを1ターン進める
+    virtual void advance(const int action) = 0;
+
+    // 現在の状況でプレイヤーが可能な行動を全て取得する
+    virtual std::vector<int> legalActions() = 0;
+};
+
+// 探索時のソート用に評価を比較する
+bool operator<(const State &state_1, const State &state_2)
+{
+    return state_1.evaluated_score_ < state_2.evaluated_score_;
+}
+
+class PyState : public State
+{
+public:
+    /* Inherit the constructors */
+    using State::State;
+
+    bool isDone() override
+    {
+        PYBIND11_OVERRIDE_PURE(/* Return type */ bool, /* Parent class */ State, /* Name of function */ isDone);
+    }
+
+    void evaluateScore() override
+    {
+        PYBIND11_OVERRIDE_PURE(/* Return type */ void, /* Parent class */ State, /* Name of function */ evaluateScore);
+    }
+
+    void advance(int action) override
+    {
+        PYBIND11_OVERRIDE_PURE(/* Return type */ void, /* Parent class */ State, /* Name of function */ advance, /* args */ action);
+    }
+
+    std::vector<int> legalActions() override
+    {
+        PYBIND11_OVERRIDE_PURE(/* Return type */ std::vector<int>, /* Parent class */ State, /* Name of function */ legalActions);
+    }
+
+    void setEvaluateScore(double evaluated_score) override
+    {
+        PYBIND11_OVERRIDE(/* Return type */ void, /* Parent class */ State, /* Name of function */ setEvaluateScore, /* args */ evaluated_score);
+    }
+};
+
+double getEvaluatedScore(State *state)
+{
+    return state->evaluated_score_;
+}
+
 namespace py = pybind11;
 
 PYBIND11_MODULE(thun_search, m)
@@ -75,6 +144,17 @@ PYBIND11_MODULE(thun_search, m)
         .def("go", &Animal::go);
 
     m.def("call_go", &call_go);
+
+    py::class_<State, PyState>(m, "State")
+        .def(py::init<>())
+        .def("isDone", &State::isDone)
+        .def("evaluateScore", &State::evaluateScore)
+        .def("advance", &State::advance)
+        .def("legalActions", &State::legalActions)
+        .def("setEvaluateScore", &State::setEvaluateScore);
+
+    m.def("getEvaluatedScore", &getEvaluatedScore);
+
 #ifdef VERSION_INFO
     m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
 #else
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -13,9 +13,42 @@ class Cat(thun.Animal):
         return "meow! " * n_times
 
 
+class MazeState(thun.State):
+    dy = [1, -1, 0, 0]
+    dx = [0, 0, 1, 1]
+
+    def __init__(self) -> None:
+        # thun.State.__init__(self)
+        super().__init__()
+        self.a = 10
+
+    def isDone(self):
+        return True
+
+    def evaluateScore(self):
+        super().setEvaluateScore(10)
+        return
+
+    def advance(self, action):
+        return
+
+    def legalActions(self):
+        return []
+
+
 if __name__ == "__main__":
     print("thun\__version__", thun.__version__)
     print([key for key in thun.__dict__.keys() if "__" != key[:2]])
     test_main()
     cat = Cat()
     print(thun.call_go(cat))
+    state = MazeState()
+    print("thun.State", [
+          key for key in thun.State.__dict__.keys() if "__" != key[:2]])
+    print("MazeState", [
+          key for key in MazeState.__dict__.keys() if "__" != key[:2]])
+
+    print("state", [
+          key for key in state.__dict__.keys() if True or "__" != key[:2]])
+    # state.evaluateScore()
+    print(thun.getEvaluatedScore(state))
