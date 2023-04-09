e13fd24666982105a34ecf05772881e3bc57c2b6
thunder <aoki.eita1130@gmail.com>
2023-04-09 16:57:18 +0900

    test_basic

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,7 +1,21 @@
-import cmake_example as m
+import thun_search as thun
+import pandas
 
 
 def test_main():
-    assert m.__version__ == "0.0.1"
-    assert m.add(1, 2) == 3
-    assert m.subtract(1, 2) == -1
+    assert thun.add(1, 2) == 3
+    assert thun.subtract(1, 2) == -1
+
+
+class Cat(thun.Animal):
+
+    def go(self, n_times):
+        return "meow! " * n_times
+
+
+if __name__ == "__main__":
+    print("thun\__version__", thun.__version__)
+    print([key for key in thun.__dict__.keys() if "__" != key[:2]])
+    test_main()
+    cat = Cat()
+    print(thun.call_go(cat))
